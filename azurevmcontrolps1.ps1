a8ccf930f627aa300fb20445060af7d8543dbb08
devilsfan655 <100380069+devilsfan655@users.noreply.github.com>
2022-07-17 17:51:15 -0400

    Made the script smarter

--- azurevmcontrolps1.ps1
+++ azurevmcontrolps1.ps1
@@ -1,4 +1,4 @@
-$Computers = Get-AzVM -ResourceGroup $args[0] 
+$Computers = Get-AzVM -ResourceGroup $args[0] -Status
 
 Write-Host "Found the following machine(s) in $($args[0]) `n" -ForegroundColor Yellow
 
@@ -15,33 +15,48 @@ ForEach ($VirtualMachine in $Computers){
 
     if ($args[1] -match 'start'){
 
-        Write-Host "Attempting to start Virtual Machine $($VirtualMachine.Name)" -ForegroundColor DarkYellow
-
-        try 
+        if ($VirtualMachine.PowerState -eq "VM deallocated")
         {
-            Start-AzVM -ResourceGroupName $args[0] -Name $VirtualMachine.Name -Confirm:$false -ErrorAction SilentlyContinue 
-            Write-Host "Successfully started $($VirtualMachine.Name)" -ForegroundColor DarkGreen
+            Write-Host "Attempting to start Virtual Machine $($VirtualMachine.Name)" -ForegroundColor DarkYellow
+            
+            try 
+            {
+                Start-AzVM -ResourceGroupName $args[0] -Name $VirtualMachine.Name -Confirm:$false -ErrorAction SilentlyContinue 
+                Write-Host "Successfully started $($VirtualMachine.Name)" -ForegroundColor DarkGreen
+            }
+            catch
+            {
+                Write-Host "Failed to start $($VirtualMachine.Name)" -ForegroundColor DarkRed
+            }
         }
-        catch
+        else
         {
-            Write-Host "Failed to start $($VirtualMachine.Name)" -ForegroundColor DarkRed
+            Write-Host "$($VirtualMachine.Name) is already running..." -ForegroundColor DarkYellow
         }
-
     }
     
     elseif ($args[1] -match 'stop') {
 
-        Write-Host "Attempting to stop Virtual Machine $($VirtualMachine.Name)" -ForegroundColor DarkYellow
-        try 
+        if ($VirtualMachine.PowerState -eq "VM running" -or $VirtualMachine.PowerState -eq "VM starting") 
         {
-            Stop-AzVM -ResourceGroupName $args[0] -Name $VirtualMachine.Name -Confirm:$false -ErrorAction SilentlyContinue -Force
-            Write-Host "Successfully stopped $($VirtualMachine.Name)" -ForegroundColor DarkGreen
-        }
-        catch 
-        {
-            Write-Host "Failed to stop $($VirtualMachine.Name)" -ForegroundColor DarkRed
-        }
 
+            Write-Host "Attempting to stop Virtual Machine $($VirtualMachine.Name)" -ForegroundColor DarkYellow
+
+            try 
+            {
+                Stop-AzVM -ResourceGroupName $args[0] -Name $VirtualMachine.Name -Confirm:$false -ErrorAction SilentlyContinue -Force
+                Write-Host "Successfully stopped $($VirtualMachine.Name)" -ForegroundColor DarkGreen
+            }
+            catch 
+            {
+                Write-Host "Failed to stop $($VirtualMachine.Name)" -ForegroundColor DarkRed
+            }
+    
+        }
+        else 
+        {
+            Write-Host "$($VirtualMachine.Name) is already stopped..." -ForegroundColor DarkYellow
+        }
 
     }
 }
